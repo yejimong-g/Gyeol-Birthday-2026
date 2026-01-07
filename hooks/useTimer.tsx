07a417e9876457e243d3fa1f179c46d75af3a8cb
yejimong-g <yejishin428@gmail.com>
2026-01-07 14:39:37 -0800

    Update useTimer.tsx

--- hooks/useTimer.tsx
+++ hooks/useTimer.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const useTimer = () => {
-  const setDate = new Date("2026-01-08T18:00:00+0900");
+  const setDate = new Date("2026-01-08T00:00:00-06:00")
 
   const now = new Date();
 
