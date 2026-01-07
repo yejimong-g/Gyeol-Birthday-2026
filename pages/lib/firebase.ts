b3dbfeb0fb87bef5752beca0f968d3e2d353c530
yejimong-g <yejishin428@gmail.com>
2026-01-07 14:54:40 -0800

    Update firebase.ts

--- pages/lib/firebase.ts
+++ pages/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
+export const firestore = getFirestore(app);
