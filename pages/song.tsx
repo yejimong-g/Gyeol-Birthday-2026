d1f65d84c00eddb9101f85c39281261955971c77
yejimong-g <yejishin428@gmail.com>
2026-01-07 14:22:06 -0800

    Update song.tsx

--- pages/song.tsx
+++ pages/song.tsx
@@ -1,27 +1,23 @@
-import React from "react";
-import Bar from "../components/Bar";
-import bday from "../public/image/bday.png";
-import Image from "next/image";
-import YouTube from "react-youtube";
-
-const song = () => {
+export default function Song() {
   return (
-    <div className="wrapper">
-      <span className="title py-20">🎶 Song</span>
-      <Bar />
-      <div className="w-[500px] pt-20">
-        <Image alt="" src={bday} objectFit="contain" />
-      </div>
-      <YouTube
-        videoId="AWrXKyFzg0o"
-        opts={{
-          width: "560",
-          height: "315",
-          playerVars: { autoplay: 1, rel: 0, modestbranding: 1 },
-        }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#000",
+      }}
+    >
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/AXfRmYr2sWE?start=32&end=212"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
       />
     </div>
   );
-};
-
-export default song;
+}
